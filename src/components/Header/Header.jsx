0bcc9f1a7002796074fcbf5a3db8c7bb2b598af1
Mahdi Hossen <114096228+0Mahdi@users.noreply.github.com>
2024-04-19 12:37:54 +0600

    header top section completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Row, Col } from 'reactstrap';
+import '../../styles/header.css';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return <header className='header'>
+    {/* ===== Header Top ====== */}
+    <div className="header__top">
+      <Container>
+        <Row>
+          <Col lg='6' md='6' sm='6'>
+            <div className="header__top__left">
+              <span>Need Help?</span>
+              <span className='header__top__help'>
+                < i class="ri-phone-fill"></i> +1-202-333-09812
+              </span>
+            </div>
+          </Col>
+
+          <Col lg='6' md='6' sm='6'>
+            <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
+              <Link to='#' className='d-flex align-items-center gap-1'> 
+                <i class="ri-login-circle-line"></i> Login
+              </Link>
+
+              <Link to='#' className='d-flex align-items-center gap-1'> 
+                <i class="ri-user-line"></i> Register
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  </header>
 }
 
 export default Header
