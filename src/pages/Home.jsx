076a6511b501eedd5742222ca84aacbeca4b761b
Mahdi Hossen <114096228+0Mahdi@users.noreply.github.com>
2024-04-25 17:33:44 +0600

    hero section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Helmet from '../components/Helmet/Helmet'
+import HeroSlider from '../components/UI/HeroSlider'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Helmet title='Home'>
+      <section className='p-0 hero__slider-section'>
+        <HeroSlider />
+      </section>
+    </Helmet>
   )
 }
 
