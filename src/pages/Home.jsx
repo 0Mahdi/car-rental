da7ef5f6302efd7669d52eb205c33cae8d438df8
Mahdi Hossen <114096228+0Mahdi@users.noreply.github.com>
2024-04-25 18:36:35 +0600

    service & car offer section completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,9 @@ import HeroSlider from '../components/UI/HeroSlider';
 import { Container, Row, Col } from "reactstrap";
 import FindCarForm from '../components/UI/FindCarForm';
 import AboutSection from '../components/UI/AboutSection';
+import ServicesList from '../components/UI/ServicesList';
+import CarItem from '../components/UI/CarItem';
+import carData from '../assets/data/carData';
 
 const Home = () => {
   return (
@@ -34,6 +37,34 @@ const Home = () => {
 
       {/* =========== about section ================ */}
       <AboutSection />
+      {/* ========== services section ============ */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+              <h6 className="section__subtitle">See our</h6>
+              <h2 className="section__title">Popular Services</h2>
+            </Col>
+
+            <ServicesList />
+          </Row>
+        </Container>
+      </section>
+      {/* =========== car offer section ============= */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center mb-5">
+              <h6 className="section__subtitle">Come with</h6>
+              <h2 className="section__title">Hot Offers</h2>
+            </Col>
+
+            {carData.slice(0, 6).map((item) => (
+              <CarItem item={item} key={item.id} />
+            ))}
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   )
 }
